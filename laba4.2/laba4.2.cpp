d3cbecf4ff908242831e57177e2af5f05180e697
Teks <Teks@TEKS>
2023-10-24 19:11:09 +0700

    Добавьте файлы проекта.

---
+++ laba4.2/laba4.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+// Вариант 8
+int main() {
+    setlocale(LC_ALL, "ru");
+    int a, b;
+    cout << "Напишите 2 целых числа a и b (a < b): ";
+    cin >> a >> b;
+    if (a > b) {
+        cerr << "Ошибка, a > b" << endl;
+        return 0;
+    }
+    long long  sum = 1;
+    for (int i = a; i <= b; i++) {
+        sum *= i;
+    }
+    cout << "Произведение всех чисел от a до b: " << sum << endl;
+}
